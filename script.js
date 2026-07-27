79718d24ab3a5e21f7e93db4a35cdcc15b57f03c
mark <njugunamark310@gmail.com>
2026-07-27 19:55:20 +0300

    Read and validate user input

--- script.js
+++ script.js
@@ -4,9 +4,19 @@ const form = document.getElementById("akanform");
 //check for form found by javascript
 console.log(form);
 
-//listen for form submission
+// listen for form submission
 form.addEventListener("submit", function (event) {
+     
+    // Stop refresh in page
     event.preventDefault();
 
-    console.log("form submitted!");
+    // Get selected birth page 
+    const birthDate = document.getElementById("birthDate").value;
+
+    // Get selected gender 
+    const gender =document.querySelector('input[name="gender"]:checked');
+
+    console.log("Birth Date:", birthDate);
+    console.log("Gender:", gender);
+
 });
